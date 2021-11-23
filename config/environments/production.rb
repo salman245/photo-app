2b01983a477f9edc3bd1cd76bb5cf5a74b099f09
Salman Tahir <salmantahir3249@gmail.com>
2021-11-23 11:49:43 +0500

    pg

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   enable_starttls_auto: true
  }
 
- config.action_mailer.default_url_options = { :host => 'https://photo-app-159.herokuapp.com', :protocol => 'https'}
+ config.action_mailer.default_url_options = { :host => 'https://photo-app-159.herokuapp.com/'}
   
 
 
